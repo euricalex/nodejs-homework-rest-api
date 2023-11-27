67dc2d1b84e1aaa4346b5b322b5e6ee0077166f8
Alexander <122707746+euricalex@users.noreply.github.com>
2023-11-27 18:56:03 +0200

    bagfix

--- routes/api/index.js
+++ routes/api/index.js
@@ -2,5 +2,5 @@ const express = require("express");
 const router = express.Router();
 const authRoutes = require("./auth");
 
-router.use("/auth", authRoutes);
+router.use("/users", authRoutes);
 module.exports = router;
